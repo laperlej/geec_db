81ad7c20514e9cbbadf5eb9b63e5680b0d9e2a4c
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-03-17 11:35:04 -0400

    added script to generate json for geec_web

--- generate_json.py
+++ generate_json.py
@@ -1,5 +1,6 @@
 import sys
 import sqlite3
+import json
 
 class IhecDb(object):
     def __init__(self, db_path):
@@ -24,13 +25,34 @@ class IhecDb(object):
                  LEFT JOIN hub_description
                  ON datasets.hub_id = hub_description.hub_id"""
         self.cursor.execute(sql)
-        print self.cursor.fetchone()
+        return self.cursor.fetchall()
+
+def list_2_json(datasets):
+    json_content = {'datasets':[]}
+    for dataset in datasets:
+        data = {
+            'assay': dataset[0],
+            'assay_category': dataset[1],
+            'cell_type': dataset[2],
+            'cell_type_category': dataset[3],
+            'analysis_group': dataset[4],
+            'file_path': dataset[5],
+            'file_name': dataset[6],
+            'md5sum': dataset[7],
+            'publishing_group': dataset[8],
+        }
+        json_content['datasets'].append(data)
+    return json.dumps(json_content, separators=(',',':'), sort_keys=True)
 
 
 def main():
     sqlite_path = sys.argv[1]
+    out_path = sys.argv[1]
     ihec_db = IhecDb(sqlite_path)
-    ihec_db.get_json_view()
+    datasets = ihec_db.get_json_view
+    json = list_2_json(datasets)
+    with open(out_path, 'w') as output_file:
+        output_file.write(json)
 
 if __name__ == '__main__':
     main()
